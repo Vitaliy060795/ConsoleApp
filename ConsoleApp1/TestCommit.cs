4635e9ce66e25e1b7787f269e6ac73b7dab96616
Vitaliy <vitaliy060795@gmail.com>
2019-06-10 22:51:56 +0300

    add new class temp

---
+++ ConsoleApp1/TestCommit.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class TestCommit
+    {
+        dynamic i = 100;
+        
+        
+        public int TempMethod(int t)
+        {
+            int i = 100 + t;
+            return i;
+        }
+    }
+}
